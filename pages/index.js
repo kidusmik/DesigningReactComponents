5ee4a15a1f6a6e4e354e74479066409b3fb66f8f
kidusmik <kidusmik@gmail.com>
2023-10-05 19:58:44 +0300

    Complete card

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,52 @@
+import { data } from "../SpeakerData"
+
 const IndexPage = () => {
+
+    // const {id,bio,first,last,favorite,twitterHandle,company,sessions,} = data[0];
     return (
-        <div>Hello From Pluralsight</div>
-    );
-}
+        <div className="container speakers-list">
+            <div className="row">
+
+                {data.map(function (speaker){
+                    const {id,bio,first,last,favorite,twitterHandle,company,sessions,} = speaker;
+
+                return (
+                    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
+                    <div className="card card-height  p-4 mt-4">
+                        <div className="speaker-img d-flex flex-row">
+                            <img 
+                                className="container-fit" 
+                                src={`/images/speaker-${id}.jpg`} 
+                                width="300" 
+                                alt="{`${first} ${last}`}"
+                            />
+                        </div>
+
+                        <div className="speaker-info">
+                            <div className="d-flex">
+                                <h3 className="text-truncate">
+                                    {first} {last}
+                                </h3>
+                            </div>
+
+                            <div>
+                                <p> {bio} {company} {twitterHandle} {favorite} </p>
+                            </div>
+                        </div>
+
+                        <div className="sessionBox">
+                            <span className="session">
+                                {sessions[0].title} <strong>Room: {sessions[0].room.name}</strong>
+                            </span>
+                        </div>
+                    </div>
+
+                </div>
+                )
+            })}
+            </div>
+        </div>
+    )
+};
 
 export default IndexPage;
